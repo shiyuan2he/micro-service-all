f498bc9a06067d28f9bdc3441bc079a22d895e86
shiyuan2he <shiyuan4work@126.com>
2021-06-18 11:27:06 +0800

    init

---
+++ config/netflix-config/src/main/java/org/hsy/cloud/register/NetflixConfigApplication.java
@@ -0,0 +1,11 @@
+package org.hsy.cloud.register;
+
+/**
+ * @author heshiyuan
+ * @date 2021/6/18 11:11
+ */
+public class NetflixConfigApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ register/netflix-eureka/src/main/java/org/hsy/cloud/register/NetflixEurekaApplication.java
@@ -0,0 +1,11 @@
+package org.hsy.cloud.register;
+
+/**
+ * @author heshiyuan
+ * @date 2021/6/18 11:12
+ */
+public class NetflixEurekaApplication {
+    public static void main(String[] args) {
+
+    }
+}
