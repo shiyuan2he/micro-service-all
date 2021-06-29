d31d37de1104985ce864482d3278f677ef97e4f1
shiyuan2he <shiyuan4work@126.com>
2021-06-29 13:52:29 +0800

    build

--- config/netflix-config-client/src/main/java/org/hsy/cloud/config/client/ConfigClientApplication.java
+++ config/netflix-config-client/src/main/java/org/hsy/cloud/config/client/ConfigClientApplication.java
@@ -2,14 +2,16 @@ package org.hsy.cloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author heshiyuan
  * @date 2021/6/29 10:20
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class, args);
+        new SpringApplication(ConfigClientApplication.class).run(args);
     }
 }
--- config/netflix-config-client/src/main/java/org/hsy/cloud/config/client/controller/ApiController.java
+++ config/netflix-config-client/src/main/java/org/hsy/cloud/config/client/controller/ApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiController {
 
-    @Value("${xxoo}")
+    @Value("${xxoo:000}")
     private String xxoo;
 
     @GetMapping("/config/info")
