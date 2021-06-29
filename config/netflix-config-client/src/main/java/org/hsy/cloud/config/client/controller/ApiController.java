de9668e8eb40724dafee305e32c1862a146be355
shiyuan2he <shiyuan4work@126.com>
2021-06-29 14:16:52 +0800

    build

--- config/netflix-config-client/src/main/java/org/hsy/cloud/config/client/controller/ApiController.java
+++ config/netflix-config-client/src/main/java/org/hsy/cloud/config/client/controller/ApiController.java
@@ -1,6 +1,7 @@
 package org.hsy.cloud.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/6/29 10:48
  */
 @RestController
+@RefreshScope
 public class ApiController {
 
     @Value("${xxoo:000}")
